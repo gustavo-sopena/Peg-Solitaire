fa24f049a7cc97473ac8fd657893f50628e10978
Gustavo Sopena <gsopena7@gmail.com>
2019-07-18 03:00:00 -0700

    Refactor configuration.py and main.py
    - Remove ability to place excel files in folder
    - Rename excel files to better represent which game(s) are played
    - Add ability to play all Z_n configurations
    - Pause ability to play half of Z_3 games
    - Format example games excel file to match configuration games excel file

--- configuration.py
+++ configuration.py
@@ -8,27 +8,48 @@ import setup
 import xlsxwriter
 import math
 
+# set the color set to use, match this in setup.py and PegGame.py as well
+# e.g., Z_3 = (0, 1, 2)
+# set the number of vertices for the graph (int)
+# set the type of graph (string)
+# for now, options are: circle, path
+n = 3
 size = 3
 type = 'circle'
 G = setup.makeGraph(size, type)
-fileName = 'peg-solitaire'+'-'+type+'-'+str(size)+'-'+'z3'+'.xlsx'
+
+# the configuration, C, does not need to set manually
+# they are going to found automatically
+
+# create the file name for this game
+# warning: if a file with the same name exists, it will override that file
+fileName = 'peg-solitaire-configuration-sheet'+'-'+type+'-'+str(size)+'-'+'z'+str(n)+'.xlsx'
 print("Saving to file: "+fileName)
 
+# open the workbook and add a worksheet (additionally, set text formatting)
+# begin row at 0
 workbook = xlsxwriter.Workbook(fileName)
 worksheet = workbook.add_worksheet()
 bold = workbook.add_format({'bold':True})
 boldright = workbook.add_format({'align':'right', 'bold':True})
-
-maxGames = math.pow(2, size-1)
-halfGames = maxGames / 2
-
 row = 0
+
+# set the total number of games
+totalGames = math.pow(n-1, size-1) * size
+# halfGames = totalGames / 2
+
+# set counter to determine number of games won
+# set counter for the current game
+# the outer 'for loop' sets the vertex position of the zero, starting at vertex 1
+# the inner 'for loop' sets the configuration for the program to play
 wonGames = 0
 gameIndex = 1
-for zeroPosition in range(1, 2):
+for zeroPosition in range(1, size+1):
+    # find the configurations based on the zero positions
     configurations = setup.findConfigurationsForGraphSize(size, zeroPosition)
 
     for config in configurations:
+        # write a header-like row in the excel file for the current game
         worksheet.write(row, 0, "Game", bold)
         worksheet.write(row, 1, gameIndex, bold)
         row += 1
@@ -39,6 +60,8 @@ for zeroPosition in range(1, 2):
 
         # play the game
         result, graph, sequence, seen = PegGame.winnable(G, config, [], [])
+
+        # show if the game won
         worksheet.write(row, 0, "Win", bold)
         worksheet.write(row, 1, str(result), boldright)
         row += 1
@@ -55,19 +78,21 @@ for zeroPosition in range(1, 2):
         #     setup.writeConfigurationToSheet(c, row, worksheet)
         #     row += 1
 
+        # if the game is winnable, then increase counter
         if result == True:
             wonGames += 1
 
+        # increase current game counter
         gameIndex += 1
         row += 1
 
         # check if we played half of the games
-        if gameIndex == halfGames + 1:
-            break
+        # if gameIndex == halfGames + 1:
+        #     break
 
 worksheet.write(row, 0, "Total")
-worksheet.write(row, 1, maxGames)
-print("Total: "+str(int(maxGames)))
+worksheet.write(row, 1, totalGames)
+print("Total: "+str(int(totalGames)))
 row += 1
 
 worksheet.write(row, 0, "Played")
--- main.py
+++ main.py
@@ -6,37 +6,59 @@
 import PegGame
 import setup
 import xlsxwriter
-import shutil
-import os
+# import math
 
-# specify the number of vertices for the graph (int)
-# specify the type of graph (string)
+# set the color set to use, match this in PegGame.py as well
+# e.g., Z_3 = (0, 1, 2)
+# set the number of vertices for the graph (int)
+# set the type of graph (string)
 # for now, options are: circle, path
+n = 3
 size = 8
 type = 'circle'
 G = setup.makeGraph(size, type)
 
-# specify the configuration for the graph
-C = {1:0, 2:2, 3:1, 4:2, 5:1, 6:2, 7:1, 8:2}
+# set the configuration for the graph
+C = {1:0, 2:2, 3:1, 4:1, 5:1, 6:2, 7:1, 8:2}
 
 # create the file name for this game
+# warning: if a file with the same name exists, it will override that file
+fileName = 'peg-solitaire-example-sheet'+'-'+type+'-'+str(size)+'-'+'z'+str(n)+'.xlsx'
+print("Saving to file: "+fileName)
+
 # open the workbook and add a worksheet
 # begin row at 0
-fileName = 'peg-solitaire-example'+'-'+type+'-'+str(size)+'-'+'z3'+'.xlsx'
 workbook = xlsxwriter.Workbook(fileName)
 worksheet = workbook.add_worksheet()
+bold = workbook.add_format({'bold':True})
+boldright = workbook.add_format({'align':'right', 'bold':True})
 row = 0
+
+# there is only one game being played
+# so, no game index for this file
+
+# write a header-like row in the excel file for the current game
+worksheet.write(row, 0, "Game", bold)
+worksheet.write(row, 1, 0, bold)
+row += 1
+
+# show the current game
+setup.writeConfigurationToSheet(C, row, worksheet)
+row += 1
 
 # play the game
 result, graph, sequence, seen = PegGame.winnable(G, C)
 
 # show if the game won
-# optional: show the starting state
 print("Win: "+str(result))
+worksheet.write(row, 0, "Win", bold)
+worksheet.write(row, 1, str(result), boldright)
+row += 1
+
+# optional: show the starting state
 # setup.prettyPrintConfiguration(C)
 
-# check if the game is winnable
-# if so, show the steps that lead to the win
+# show series of moves that won the game, if any
 for c in sequence:
     setup.prettyPrintConfiguration(c)
     setup.writeConfigurationToSheet(c, row, worksheet)
@@ -47,11 +69,4 @@ for c in sequence:
 # for c in seen:
 #     setup.prettyPrintConfiguration(c)
 
-# close the workbook
-# move the excel sheet into the folder "Sheets"
 workbook.close()
-
-try:
-    shutil.move(fileName, 'Sheets/')
-except shutil.Error as serr:
-    os.remove(fileName)
